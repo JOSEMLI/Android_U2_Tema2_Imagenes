3147800b64f65547099de3d929b8962f923a921f
Jose Mamani <JOSEMLI@example.com>
2020-02-04 19:28:06 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android_u2_tema2_imgenes/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.android_u2_tema2_imgenes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+
+  public void imageloader(View view) {
+    startActivity(new Intent(this, MiImageLoader.class));
+  }
+}
---
+++ app/src/main/java/com/example/android_u2_tema2_imgenes/MiPicasso.java
@@ -0,0 +1,14 @@
+package com.example.android_u2_tema2_imgenes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MiPicasso extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_mi_picasso);
+  }
+}
